e5fd0721dfa850d493dd9c165062b53cf39f5701
Mateus Caires <caires@outlook.com>
2024-12-09 22:52:23 -0300

    Ajustes no use case, não subia a aplicação

--- src/main/java/br/com/fiap/restaurante/usecase/reserva/impl/FinalizarReservaUseCaseImpl.java
+++ src/main/java/br/com/fiap/restaurante/usecase/reserva/impl/FinalizarReservaUseCaseImpl.java
@@ -2,11 +2,12 @@ package br.com.fiap.restaurante.usecase.reserva.impl;
 
 import br.com.fiap.restaurante.domain.Reserva;
 import br.com.fiap.restaurante.gateway.reserva.ReservaGateway;
-import br.com.fiap.restaurante.usecase.reserva.AtualizarReservaUseCase;
+import br.com.fiap.restaurante.usecase.reserva.FinalizarReservaUseCase;
+
 import org.springframework.stereotype.Service;
 
 @Service
-public class FinalizarReservaUseCaseImpl implements AtualizarReservaUseCase {
+public class FinalizarReservaUseCaseImpl implements FinalizarReservaUseCase {
     private final ReservaGateway reservaGateway;
 
     public FinalizarReservaUseCaseImpl(ReservaGateway reservaGateway) {
